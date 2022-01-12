84a4a630cd599c0506d10e039e6616b2250af6e4
Timothé Gélibert <tim.gelibert@protonmail.com>
2022-01-12 13:49:06 +0100

    Added validation of the contact group for Client and Event creation.

--- crm/serializers.py
+++ crm/serializers.py
@@ -1,12 +1,20 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, ValidationError
 
-from .models import Client, Contract, ContractStatus, Event
+from .constants import SELLING_TEAM_NAME, SUPPORT_TEAM_NAME
+from .models import Client, Contract, ContractStatus, Event, User
 
 
 class ClientSerializer(ModelSerializer):
     class Meta:
         model = Client
         fields = "__all__"
+
+    def validate_contact(self, value):
+        """ Check if the contact is in the Selling Team. """
+        user = value
+        if user.groups.filter(name=SELLING_TEAM_NAME).exists():
+            return value
+        raise ValidationError("The 'contact' must be a Selling Team member.")
 
 
 class ContractSerializer(ModelSerializer):
@@ -25,3 +33,10 @@ class EventSerializer(ModelSerializer):
     class Meta:
         model = Event
         fields = "__all__"
+
+    def validate_support_contact(self, value):
+        """ Check if the contact is in the Selling Team. """
+        user = value
+        if user.groups.filter(name=SUPPORT_TEAM_NAME).exists():
+            return value
+        raise ValidationError("The 'support_contact' must be a Support Team member.")
